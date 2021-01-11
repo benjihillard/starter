94540f1689d7201b3b56ad9076fc017e16029896
Benjamin Hillard <51002483+benjihillard@users.noreply.github.com>
2021-01-11 15:05:06 -0600

    starter

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Popup from './components/Popup';
+function App() {
+
+  
+  
+  return (
+    
+    <div className="">
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/reducer.js
@@ -0,0 +1,16 @@
+export const initialState = {
+    
+};
+
+const reducer = (state, action) => {
+    console.log(action);
+
+    switch(action.type){
+
+        
+        default:
+            return state;
+    }
+}
+
+export default reducer;
